dbcb99c1b129a60cbef42e4142faccadd354364b
eden0330 <edadani1415@gmail.com>
2026-02-05 22:04:58 +0300

    backend and frontend clonedeployed

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const axiosinstance = axios.create({
   //deployed version of amazon server on render
 
-  baseURL: "https://amazon-clone-deploy-e1nc.onrender.com",
+  // baseURL: "https://amazon-clone-deploy-e1nc.onrender.com",
+  baseURL: "https://amazon-api-clone-deploy-2.onrender.com"
 });
 export { axiosinstance };
